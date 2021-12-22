5cea08a597e51cdfaa1f3ca033ad2f94d02c46ff
Daniel <daniel.afilipoaie@gmail.com>
2021-12-22 22:52:34 +0200

    Pig game

--- Pig Game/script.js
+++ Pig Game/script.js
@@ -1,6 +1,8 @@
 'use strict';
 
 //Selecting elements
+const player0El = document.querySelector('.player--0');
+const player1El = document.querySelector('.player--1');
 const score0El = document.querySelector('#score--0');
 const score1El = document.getElementById('score--1');
 const current0El = document.getElementById('current--0');
@@ -15,7 +17,15 @@ score0El.textContent = 0;
 score1El.textContent = 0;
 diceEl.classList.add('hidden');
 
+const scores = [0, 0];
 let currentScore = 0;
+let activePlayer = 0;
+const switchPlayer = function () {
+  document.getElementById(`current--${activePlayer}`).textContent = 0;
+  currentScore = 0;
+  player0El.classList.toggle('player--active');
+  player1El.classList.toggle('player--active');
+};
 
 //Rolling dice functionality
 btnRoll.addEventListener('click', function () {
@@ -29,8 +39,27 @@ btnRoll.addEventListener('click', function () {
   if (dice !== 1) {
     //add dice to the current score
     currentScore += dice;
-    current0El.textContent = currentScore; //change later
+    document.getElementById(`current--${activePlayer}`).textContent =
+      currentScore;
+
+    // current0El.textContent = currentScore; //change later
   } else {
     //switch to next player
+    switchPlayer();
   }
+
+  activePlayer = activePlayer === 0 ? 1 : 0;
+});
+
+btnHold.addEventListener('click', function () {
+  //1.add current score to active player score
+  scores[activePlayer] += currentScore;
+  // scores[1]=scores[1]+currentScore
+  document.getElementById(`score--${activePlayer}`).textContent =
+    scores[activePlayer];
+  //2.check if player's score is>=100
+  //finish the game
+
+  //Switch to the next player
+  switchPlayer();
 });
