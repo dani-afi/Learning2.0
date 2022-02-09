475ee8a61dff690ad17962f0286ed1a1d3721fe5
Daniel <daniel.afilipoaie@gmail.com>
2022-02-09 13:19:33 +0200

    A closer look at functions

--- 10-Functions/starter/script.js
+++ 10-Functions/starter/script.js
@@ -170,11 +170,129 @@
 // console.log(addVAT2(23));
 
 //Challenge
-const poll = {
-  question: 'What is your favourite program language?',
-  options: ['0:JavaScript', '1:Phyton', '2:Rust', '3:C++'],
-  answers: new Array(4).fill(0),
-  registerNewAanswer() {
-    const answer = prompt;
-  },
-};
+// const poll = {
+//   question: 'What is your favourite program language?',
+//   options: ['0:JavaScript', '1:Phyton', '2:Rust', '3:C++'],
+//   answers: new Array(4).fill(0),
+//   registerNewAnswer() {
+//     //Get the andswer
+//     const answer = Number(
+//       prompt(
+//         `${this.question}\n${this.options.join('\n')}\n(Write option number)`
+//       )
+//     );
+//     console.log(answer);
+//     //Register the asnwer
+//     typeof answer === 'number' &&
+//       answer < this.answers.length &&
+//       this.answers[answer]++;
+//     this.displayResults();
+//     this.displayResults('string');
+//     console.log(this.answers);
+//   },
+//   displayResults(type = 'array') {
+//     if (type === 'array') {
+//       // console.log(this.answers);
+//     } else if (type === 'string') {
+//       //Poll results are 13,2,4,1
+//       console.log(`Poll results are ${this.answers.join(', ')}`);
+//     }
+//   },
+// };
+// // poll.registerNewAanswer();
+// document
+//   .querySelector('.poll')
+//   .addEventListener('click', poll.registerNewAnswer.bind(poll));
+
+// poll.displayResults.call({ answers: [5, 3, 2] }, 'string');
+
+// const a = 7200;
+// const b = 0.1;
+// let c = 0;
+// for (let i = 1; i <= 30; i++) {
+//   c += a + a * b;
+//   console.log(`${i}:${a * i}:${c}`);
+// }
+
+// [5, 3, 2][(1, 5, 3, 9, 6, 1)];
+
+// const runOnce = function () {
+//   console.log('This will never run again');
+// };
+// runOnce();
+
+// //imediatly invoked function expresion IIEF
+// (function () {
+//   console.log('This will never run again');
+// })();
+
+// (() => console.log('This will ALSO never run again'))();
+
+// {
+//   const isPrivate = 23;
+//   var notPrivate = 45;
+// }
+// console.log(isPrivate);
+// console.log(notPrivate);
+
+// const secureBooking = function () {
+//   let passengerCount = 0;
+
+//   return function () {
+//     passengerCount++;
+//     console.log(`${passengerCount}passengers`);
+//   };
+// };
+
+// const booker = secureBooking();
+// booker();
+// booker();
+// booker();
+
+// console.dir(booker);
+
+//Example 1
+// let f;
+// const g = function () {
+//   const a = 23;
+//   f = function () {
+//     console.log(a * 2);
+//   };
+// };
+
+// const h = function () {
+//   const b = 777;
+//   f = function () {
+//     console.log(b * 2);
+//   };
+// };
+
+// g();
+// f();
+// //Re-asign f function
+// h();
+// f();
+// console.dir(f);
+
+// //Example 2
+// const boardPassengers = function (n, wait) {
+//   const perGroup = n / 3;
+
+//   setTimeout(function () {
+//     console.log(`We are now boardingall ${n} passengers`);
+//     console.log(`There are 3 groups, each with ${perGroup} passengers`);
+//   }, wait * 1000);
+
+//   console.log(`Will start boarding in ${wait} seconds`);
+// };
+// const perGroup = 1000;
+// boardPassengers(180, 3);
+
+//Coding ghallenge
+(function () {
+  const header = document.querySelector('h1');
+  header.style.color = 'red';
+  document.querySelector('body').addEventListener('click', function () {
+    header.style.color = 'blue';
+  });
+})();
